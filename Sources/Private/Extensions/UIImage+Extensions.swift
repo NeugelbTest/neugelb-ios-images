2fee734fd66a4ba4bf065d97bdf515c3b0fc1f66
Matt Moayeri <notron021@gmail.com>
2025-03-09 22:47:02 +0100

    Added Images

---
+++ Sources/Private/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+extension UIImage {
+    
+    func tinted(with color: UIColor, isOpaque: Bool = false) -> UIImage? {
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: size, format: format).image { _ in
+            color.set()
+            withRenderingMode(.alwaysTemplate).draw(at: .zero)
+        }
+    }
+    
+    func withInset(_ insets: UIEdgeInsets) -> UIImage? {
+      let cgSize = CGSize(
+        width: size.width + insets.left * scale + insets.right * scale,
+        height: size.height + insets.top * scale + insets.bottom * scale
+      )
+
+      UIGraphicsBeginImageContextWithOptions(cgSize, false, scale)
+      defer { UIGraphicsEndImageContext() }
+
+      let origin = CGPoint(x: insets.left * scale, y: insets.top * scale)
+      self.draw(at: origin)
+
+      return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(renderingMode)
+    }
+    
+}
