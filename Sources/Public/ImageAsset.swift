2bf148e0ab79ea25435b21058b83eeb10207e3a4
Matt Moayeri <notron021@gmail.com>
2025-03-09 22:53:33 +0100

    Add images (#1)
    * Added Images
    
    * Added tests
    
    * Ran linter

---
+++ Sources/Public/ImageAsset.swift
@@ -0,0 +1,73 @@
+import SwiftUI
+import UIKit
+
+public enum ImageAsset {
+
+    nonisolated(unsafe) public static var onError: ((String) -> Void)?
+
+    public static var calendar: Asset { Asset(name: "ic_calendar") }
+    public static var clock: Asset { Asset(name: "ic_clock") }
+    public static var loading: Asset { Asset(name: "ic_loading") }
+    public static var search: Asset { Asset(name: "ic_search") }
+    public static var ticket: Asset { Asset(name: "ic_ticket") }
+    public static var star: Asset { Asset(name: "ic_star") }
+    public static var imageLoading: Asset { Asset(name: "img_Loading") }
+    public static var imagePlaceholder: Asset { Asset(name: "img_placeholder") }
+
+}
+
+// MARK: - Image Asset
+
+public extension ImageAsset {
+
+    struct Asset: Hashable {
+
+        let name: String
+
+        public var uiImage: UIImage {
+            guard let result = UIImage(named: name, in: .module, with: nil) else {
+                ImageAsset.onError?("Could not find image named \(name) in assets")
+                return UIImage()
+            }
+            return result
+        }
+
+        public var image: Image {
+            .init(name, bundle: .module)
+        }
+
+        public func uiImage(tintColor: UIColor, isOpaque: Bool) -> UIImage? {
+            uiImage.tinted(with: tintColor, isOpaque: isOpaque)
+        }
+
+        public func uiImage(tintColor: UIColor) -> UIImage {
+            uiImage.withTintColor(tintColor, renderingMode: .alwaysOriginal)
+        }
+
+        public func uiImage(tintColor: UIColor, inset: CGFloat) -> UIImage {
+            uiImage(
+                tintColor: tintColor,
+                insets: .init(top: inset, left: inset, bottom: inset, right: inset)
+            )
+        }
+
+        public func uiImage(tintColor: UIColor, insets: UIEdgeInsets) -> UIImage {
+            guard let image = UIImage(named: name, in: .module, with: nil)?.withTintColor(tintColor, renderingMode: .alwaysOriginal) else {
+                ImageAsset.onError?("Could not find image named \(name) in assets")
+                return UIImage()
+            }
+            if let insettedImage = image.withInset(insets) {
+                return insettedImage
+            } else {
+                ImageAsset.onError?("Could not create insetted image from asset named \(name)")
+                return image
+            }
+        }
+
+        public func uiImage(renderingMode: UIImage.RenderingMode) -> UIImage {
+            uiImage.withRenderingMode(renderingMode)
+        }
+
+    }
+
+}
--- Tests/NeugelbImagesTests/NeugelbImagesTests.swift
+++ Tests/NeugelbImagesTests/NeugelbImagesTests.swift
@@ -1,6 +1,46 @@
 import Testing
+import SwiftUI
 @testable import NeugelbImages
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct ImageAssetTests {
+
+    @Test
+    func test_asset_names() {
+        #expect(ImageAsset.calendar.name == "ic_calendar")
+        #expect(ImageAsset.clock.name == "ic_clock")
+        #expect(ImageAsset.loading.name == "ic_loading")
+        #expect(ImageAsset.search.name == "ic_search")
+        #expect(ImageAsset.ticket.name == "ic_ticket")
+        #expect(ImageAsset.star.name == "ic_star")
+        #expect(ImageAsset.imageLoading.name == "img_Loading")
+        #expect(ImageAsset.imagePlaceholder.name == "img_placeholder")
+    }
+
+    @Test
+    func test_UIImage_retrieval() {
+        let asset = ImageAsset.calendar
+        let image = asset.uiImage
+        #expect(image != nil, "UIImage should not be nil")
+    }
+
+    @Test
+    func test_UIImage_with_tint_color() {
+        let asset = ImageAsset.clock
+        let tinted = asset.uiImage(tintColor: .red)
+        #expect(tinted != nil, "UIImage with tint color should not be nil")
+    }
+
+    @Test
+    func test_UIImage_with_rendering_mode() {
+        let asset = ImageAsset.ticket
+        let renderedImage = asset.uiImage(renderingMode: .alwaysTemplate)
+        #expect(renderedImage != nil, "UIImage with rendering mode should not be nil")
+    }
+
+    @Test
+    func test_UIImage_Wwith_insets() {
+        let asset = ImageAsset.imagePlaceholder
+        let insetImage = asset.uiImage(tintColor: .blue, inset: 10)
+        #expect(insetImage != nil, "UIImage with insets should not be nil")
+    }
 }
